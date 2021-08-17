463fe7481ee09156d06c97f2ed4af12cec397e86
mc1098 <m.cripps1@uni.brighton.ac.uk>
2021-08-17 20:05:28 +0100

    Fix doc tests
    Use new event listener syntax

--- packages/yew/src/html/listener/mod.rs
+++ packages/yew/src/html/listener/mod.rs
@@ -41,7 +41,7 @@ where
     /// fn view(&self, ctx: &Context<Self>) -> Html {
     ///     html! {
     ///         <div
-    ///             onchange={ctx.link().batch_callback(|e: Event| {
+    ///             on:change={ctx.link().batch_callback(|e: Event| {
     ///                 if let Some(input) = e.target_dyn_into::<HtmlTextAreaElement>() {
     ///                     Some(Msg::Value(input.value()))
     ///                 } else {
@@ -98,7 +98,7 @@ where
     /// fn view(&self, ctx: &Context<Self>) -> Html {
     ///     html! {
     ///         <input type="text"
-    ///             onchange={ctx.link().callback(|e: Event| {
+    ///             on:change={ctx.link().callback(|e: Event| {
     ///                 // Safe to use as callback is on an `input` element so this event can
     ///                 // only come from this input!
     ///                 let input: HtmlInputElement = e.target_unchecked_into();
